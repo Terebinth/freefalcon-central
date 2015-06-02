9de314fd9bc2e929c014753dfea17845033c0125
wylie <joshua.cullick@gmail.com>
2015-06-02 12:40:05 +0300

    ...

--- dev_ops_tools/code_reez/matrix.cpp
+++ dev_ops_tools/code_reez/matrix.cpp
@@ -11,7 +11,6 @@
 void MatrixMult_Generic(const Trotation* S1, const Trotation* S2, Trotation* T);
 
 
-
 void MatrixMult_3DNow(const Trotation* S1, const Trotation* S2, Trotation* T);
 
 void Matrix_Generic(const Trotation* M, const float k, Trotation* T);
---
+++ dev_ops_tools/code_reez/vector4.h
@@ -0,0 +1,19 @@
+
+
+
+#ifndef _VECTOR4_H
+
+#define _VECTOR_H
+
+#include "math.h"
+#include <float.h>
+#include "vector3.h"
+
+struct vector4
+{
+  enum component
+  {
+    X = (1 << 0),
+  }
+
+};
